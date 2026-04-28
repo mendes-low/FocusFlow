fcabdaa59859dd0d2d7942f5bae5b432f0e88689
mendes <uskembaev07@icloud.com>
2026-04-28 08:05:20 +0500

    Add Button ui component

---
+++ src/shared/ui/Button/Button.jsx
@@ -0,0 +1,11 @@
+import "./Button.scss";
+
+function Button({ children, className = "", ...props }) {
+    return (
+        <button className={`btn ${className}`} {...props}>
+            {children}
+        </button>
+    );
+}
+
+export default Button;
